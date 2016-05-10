7823768110babe4a6870bd43de14900623094921
Noora Al-Mana <nalmana.babson@gmail.com>
2016-05-09 21:10:55 -0400

    Added styles and second input with DRY code

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
 	$("h1").css('background-color', 'blue');
 
 	// Respond to a click of button-1
-	var button = $('#button-1');
-	button.click(function() {
+	$('#button-1').click(function() {
 		var value = $("#field-1").val();
-		console.log(value);
-		$("#display").append("<li class='input-1'>" + value + "</li>");
+		display(value);
+	});
+	$('#button-2').click(function() {
+		var value = $("#field-2").val(); 
+		display(value);
 	});
 
+	function display(thing) {
+		$("#display").append("<li class='input-1'>" + thing + "</li>");
+	};
 });
