49f454a8fe88b6bdab02ce5b912e790bb3327233
Noora Al-Mana <nalmana.babson@gmail.com>
2016-05-11 23:21:30 -0400

    clear input fields upon click and clear display container

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-	$("h1").css('background-color', 'blue');
+	var heading = $('h1');
+	heading.click(function(){
+		heading.animate({'margin-left': '100px'}, 2000);
+	});
 
 	// Respond to a click of button-1
 	$('#button-1').click(function() {
 		var value = $("#field-1").val();
 		display(value);
+		$("#field-1").val("");
 	});
 	$('#button-2').click(function() {
 		var value = $("#field-2").val(); 
 		display(value);
+		$("#field-2").val("");
+	});
+
+	$('#button-3').click(function() {
+		$('.input-1').hide(); 
 	});
 
 	function display(thing) {
